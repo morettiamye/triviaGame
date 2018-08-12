e0e4a6adf2a2105bea636988e78c03c5cbe2d5aa
Amy Moretti <amyemoretti@gmail.com>
2018-08-12 15:49:54 -0500

    Updates on updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,70 @@
+//Defining variables
+var correct = 0;
+var incorrect = 0;
+
+
 // Object for questions/answers
 
 var quizQuestions = [
     {
-        question1: "What is Usagi Tsukino's true identity?",
-        choices1: ["Sailor Moon", "Sailor Mercury", "Sailor Venus", "Queen Beryl"],
+        question: "What is Usagi Tsukino's true identity?",
+        choice: [ "Sailor Moon", "Sailor Venus", "Sailor Mercury", "Queen Beryl"],
+        correct: 1,
+    },   
+   
+    {
+        question: "Who is Chibi-Usa's mother?",
+        choice: [ "Sailor Moon", "Princess Serenity,", "Queen Serenity", "Neo-Queen Serenity"],
+        correct: 3,
     },
 
     {
-        question2: "Who are the guardian cats?",
-        choices2: ["Naru and Umino", "Luna and Artemis", "Haruka and Michiru, Phobos and Deimos"],
+        question: "Who are the guardian cats?",
+        choice: ["Naru & Umino", "Luna & Artemis", "Haruka & Michiru", "Phobos & Deimos"],
+        correct: 2,
     },
 
     {
-        question3: "Who is Chibi-Usa's mother?",
-        choices3: ["Princess Serenity", "Queen Serenity", "Neo Queen Serenity", "Sailor Venus"],
+        question: "Who is Usagi's true love?",
+        choice: [ "Seiya", "Motoki", "Haruka", "Mamoru"],
+        correct: 4,
     },
-
+    
     {
-        question4: "Who is Usagi's true love?",
-        choices4: ["Mamoru", "Seiya", "Motoki", "Haruka"],
-
-    },
-
-    {
-        question5: "Who is the Sailor Guardian of rebirth?",
-        choices5: ["Sailor Pluto", "Sailor Saturn", "Sailor Uranus", "Sailor Neptune"],
-    },
+        question: "Who is the Sailor Guardian of Rebirth?",
+        choice: [ "Sailor Pluto", "Sailor Uranus", "Sailor Saturn", "Sailor Neptune"],
+        correct: 3,
+    }
 ]
 
 
-// Display Questions/answers
-$(".questions").text(quizQuestions[0].question1);
+
+//Rules
+$(".rules").html("<p>Click the start button to begin the quiz.  You get 30 seconds to answer every question.  After 30 seconds the screen will clear and you can see how many answers you got wrong, and how many you got right. Can you get every question right?!")
+
 
 // Timer with start button
-
 $(".timer").html("<button>Start Game</button>");
 
 $("button").on("click", function timeUp() {
     var setTime = setTimeout(gameOver, 6000);  
     console.log("button is working");
+    // Display Questions
+    for (let i = 0; i < quizQuestions.length; i++) {
+        $(".question" +(i+1)).html("<p>" + quizQuestions[i].question);
+    for (let j = 0; j < quizQuestions[i].choice.length; j++) {
+        $(".form" +(i+1)).append("<input type='radio' name=\"" + quizQuestions[i].question + "\" + value=\"" + quizQuestions[i].choice[j].guess + "\" >" + "\t" + quizQuestions[i].choice[j].guess + "\t");
+        $(".form" +(i+1)).append("<br>");
+       
+    }
+    
+}
 });
 
 function gameOver () {
     console.log("times up")
-    $(".questions").append("You got some questions right");
-}
+    $("#questions").html("<p>" + "Times Up! Let's see your score..");
+    $("#questions").append("<p>" + "You got " + correct + " questions right");
+    $("#questions").append("<p>" + "You got " + incorrect + " questions wrong");
 
-
-
-// Display Score (Create to replace questions)
-
-
+};
